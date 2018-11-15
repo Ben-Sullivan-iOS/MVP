703f5c80db81ddd753c46ad403a7744d14e08489
Ben Sullivan <benpetersullivan@gmail.com>
2018-11-15 21:10:17 +0000

    Created project

---
+++ CrowdScoreDemoApp/CrowdScoreDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrowdScoreDemoApp
+//
+//  Created by Ben Sullivan on 15/11/2018.
+//  Copyright © 2018 Ben Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
